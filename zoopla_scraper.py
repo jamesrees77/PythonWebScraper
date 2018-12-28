4e3cf5042e463b876e4e95e532c0e88147dcd464
jamesrees77 <james2.rees@live.uwe.ac.uk>
2018-12-28 15:26:36 +0000

    adding number of bedroom && number of bathrooms to firebase

--- zoopla_scraper.py
+++ zoopla_scraper.py
@@ -7,22 +7,45 @@ cred = credentials.Certificate('/Users/james/Desktop/WebScraperProject/pythonscr
 default_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-# ZOOPLA
+# loop through amount of pages in query
 for i in range(1):
     source = requests.get('https://www.zoopla.co.uk/to-rent/property/bristol/?identifier=bristol&page_size=10&q=Bristol&search_source=to-rent&radius=0&price_frequency=per_month&pn=' + str(i)).text
 
     soup = BeautifulSoup(source, 'lxml')
 
+    # loop through all classes of listing-results-wrapper
     for results in soup.find_all('div', class_='listing-results-wrapper'):
 
         right_results = results.find('div', class_='listing-results-right')
         left_results = results.find('div', class_='listing-results-left')
 
-        property_price = right_results.a.text
+        # get number of beds
+        try:
+            number_beds = right_results.h3.find('span', class_='num-beds').text
+        except Exception as e:
+            number_beds = None
 
+        print("number of beds: ", number_beds)
+
+        # get number of bathrooms
+        try:
+            number_baths = right_results.h3.find('span', class_='num-baths').text
+        except Exception as e:
+            number_baths = None
+
+        print("number of bathrooms: ", number_baths)
+
+        # rent price of property
+        property_price = right_results.a.text
+        print("rent amount: ", property_price)
+
+        # get full address
         scraped_address = right_results.find('a', class_='listing-results-address').text
+
         # remove postcode from address
         address = scraped_address[:-4]
+        print("address: ", address)
+
         # take last 4 characters of address for post code
         postcode = scraped_address[-4:]
 
@@ -30,22 +53,29 @@ for i in range(1):
         if postcode[0] == " ":
             postcode = postcode[-3:]
 
+        print("postcode: ", postcode)
         # Ensure that property image exists - if not return None
         try:
             property_image = left_results.div.a.img['src']
         except Exception as e:
             property_image = None
 
-            print(property_image)
+        print("Property Image: ", property_image)
 
+        # point to document called properties in firebase
         doc_ref = db.collection(u'properties').document()
+        #  set object and push to firebase
         doc_ref.set({
-            u'property_photo': property_image,
             u'property_address': address,
+            u'number_of_beds': number_beds,
+            u'number_of_baths': number_baths,
             u'property_rent': property_price,
-            u'post_code': postcode
+            u'post_code': postcode,
+            u'property_photo': property_image,
         })
+        print('')
+        print('')
+        print('')
 
-
-
-# RIGHT MOVE
+#  run statement when all uploaded to firebase
+print('all properties successfully uploaded to firebase')
